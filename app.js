57a20fe0fa1a31230afe1d07971b2257e0dfaf2d
WILSON <mw0415436@otc.edu>
2018-08-29 09:41:18 -0500

    updated for fun 08/29

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("hello javascript");
+// Variable Scope
+/* multi line
+Michael Wilson
+*/
+
+const x = "none";
+switch (x) {
+    case "abc":
+    console.log("x = abc");
+    //break;
+    case "def":
+        console.log("x = def");
+        break;
+    default:
+        console.log("otherwise");
+
+}
